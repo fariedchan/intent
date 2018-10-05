bd5a57fb694471fded018fa75fe14de2ef378ca6
faried <moxet.jiensoo@gmail.com>
2018-10-05 08:11:39 +0700

    tambah halaman dan intent

--- app/src/main/java/com/example/faried/intent/MainActivity.java
+++ app/src/main/java/com/example/faried/intent/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.faried.intent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btpindah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btpindah =(Button)findViewById(R.id.pindah);
+
+        btpindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ipindah = new Intent (MainActivity.this, halaman2activity.class);
+                startActivity(ipindah);
+
+            }
+        });
     }
 }
